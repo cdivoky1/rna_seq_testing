98ec85f4b28a63c8bbdddae2da441f26b54db5e9
Michael Freitas <mike.freitas@gmail.com>
2020-03-24 15:55:33 -0400

    debug Rstudio in Jypter

--- install.R
+++ install.R
@@ -1,13 +1,18 @@
 ### install regular packages
 
-# install.packages("reticulate") # python support in RMarkdown
-# install.packages("ggplot2") # for plotting
-# install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+install.packages("ggplot2") # for plotting
+install.packages("dplyr")
+install.packages("reshape2")
+install.packages("stringr")
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+
 
 ### install bioconductor packages
 install.packages("BiocManager")
-BiocManager::install(c("reticulate","ggplot2"."rmarkdown", "caTools", "bitops","devtools")) # for knitting
+BiocManager::install() # for knitting
 
 ### install GitHub packages (tag = commit, branch or release tag)
-# install.packages("devtools")
+install.packages("devtools")
 # devtools::install_github("user/repo", ref = "tag")
